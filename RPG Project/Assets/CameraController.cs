be4bf44d1629566e3769a5489a9242aa4475e46f
hellfreezes <hellfreezes@yandex.ru>
2017-08-18 10:01:07 +0700

    Player & Camera

---
+++ RPG Project/Assets/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;                // цель камеры
+    public Vector3 offset;                  // отступ от цели
+    public float zoomSpeed = 4f;            // скорость зума
+    public float minZoom = 5f;              // минимальный зум
+    public float maxZoom = 15f;             // максимальный зум
+    public float yawSpeed = 100f;           // скорость поворота камеры
+    public float pitch = 2f;                // наклон камеры
+
+    private float currentZoom = 10f;        // текущий зум
+    private float currentYaw = 0f;          // текущий поврот камеры
+
+    void Update()
+    {
+        // рассчитываем зум
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+        // рассчитываем поврот учитывая значения горизонтальной оси ввода (клавиши стрелки влево/вправо, A, D)
+        currentYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+    }
+
+    void LateUpdate () {
+        // применяем перемещение камеры и зум
+        transform.position = target.position - offset * currentZoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
+
+        // применяем поворот
+        transform.RotateAround(target.position, Vector3.up, currentYaw);
+	}
+}
--- RPG Project/Assets/PlayerController.cs
+++ RPG Project/Assets/PlayerController.cs
@@ -2,26 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour {
+    public LayerMask movementMask;
 
-    Camera cam;
+    private Camera cam;
+    private PlayerMotor motor;
 
 	// Use this for initialization
 	void Start () {
         cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        //Ловим нажатие левой кл мыши
 		if (Input.GetMouseButtonDown(0))
+        {
+            //Строим луч через место клика мыши в 3д мир
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            //получаем координаты клика мыши в мире если клик пришелся на объект с movementMask
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                //Двигаемся в сторону объекта по которому нажали
+                motor.MoveToPoint(hit.point);
+
+                //Убираем фокус со всех объектов
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit, 100))
             {
 
             }
         }
-	}
+    }
 }
